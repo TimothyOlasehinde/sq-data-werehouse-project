8c2637775eb61da4c16e3c0965b8c1666c91cdc4
TimothyOlasehinde <olasehindetimothy@yahoo.com>
2025-07-16 20:06:24 -0600

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -115,37 +115,43 @@ BEGIN
 		RAISE INFO '-------------------------------------------------------------------------------';
 
 		s_starttime = NOW();
-		RAISE INFO '>> Truncating Table: silver.crm_prd_info';
-		TRUNCATE TABLE  silver.crm_prd_info;
-		RAISE INFO '>> Inserting Data Into Table: silver.crm_prd_info';
-		INSERT INTO silver.crm_prd_info(
-			prd_id,
-			cat_id,
-			prd_key,
-			prd_nm,
-			prd_cost,
-			prd_line,
-			prd_start_dt,
-			prd_end_dt
+		RAISE INFO '>> Truncating Table: silver.crm_sales_details';
+		TRUNCATE TABLE  silver.crm_sales_details;
+		RAISE INFO '>> Inserting Data Into Table: silver.crm_sales_details';
+		INSERT INTO silver.crm_sales_details(
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
 		)
 		SELECT 
-			prd_id,
-			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') cat_id, -- Extract category ID
-			SUBSTRING(prd_key, 7, LENGTH(prd_key)) prd_key,     -- Extract product key
-			prd_nm,
-			COALESCE(prd_cost, 0) prd_cost,
-			CASE UPPER(TRIM(prd_line))
-				 WHEN 'M' THEN 'Mountain'
-				 WHEN 'R' THEN 'Road'
-				 WHEN 'S' THEN 'Other Sales'
-				 WHEN 'T' THEN 'Touring'
-				 ELSE 'n/a'
-			END prd_line, -- Map product line codes to descriptive values
-			prd_start_dt,
-			CAST(
-				LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) -1 AS DATE
-				) AS prd_end_dt -- Calculate end date as one day before the next start date
-		FROM bronze.crm_prd_info;
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			CASE WHEN sls_order_dt = 0 OR LENGHT(sls_order_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+			END sls_order_dt,
+			CASE WHEN sls_ship_dt = 0 OR LENGHT(sls_ship_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+			END sls_ship_dt,
+			CASE WHEN sls_due_dt = 0 OR LENGHT(sls_due_dt) != 8 THEN NULL
+				ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+			END sls_due_dt,
+			CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+				THEN sls_quantity * ABS(sls_price)
+				ELSE sls_sales
+			END sls_sales, -- Recalculate sales if original value is missing or incorrect
+			sls_quantity,
+			CASE WHEN sls_price IS NULL OR sls_price <= 0 
+				THEN sls_sales / NULLIF(sls_quantity, 0)
+				ELSE sls_price  -- Derive price if original value is invalid
+			END sls_price
+		FROM bronze.crm_sales_details;
 		e_endtime = NOW();
 		RAISE INFO '>>Load Duration: ';
 		RAISE INFO '%', CAST(e_endtime - s_starttime AS VARCHAR); 
@@ -235,8 +241,8 @@ BEGIN
 
 END $$;
 
-CALL silver.load_silver();
 
+CALL silver.load_silver();
 
 
 
